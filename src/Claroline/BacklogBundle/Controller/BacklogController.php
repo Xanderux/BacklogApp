f97a3cbf083a6b8111408bb52bd75c35894cd523
Stéphane Klein <stephaneklein221@gmail.com>
2014-04-02 10:03:51 +0200

    Added first views

--- src/Claroline/BacklogBundle/Controller/BacklogController.php
+++ src/Claroline/BacklogBundle/Controller/BacklogController.php
@@ -4,18 +4,29 @@ namespace Claroline\BacklogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
-// these import the "@Route" and "@Template" annotations
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration as EXT;
 
 class BacklogController extends Controller
 {
     /**
-     * @Route("/", name="index")
-     * @Template()
+     * @EXT\Route("/tickets", name="tickets")
+     * @EXT\Template
      */
-    public function indexAction()
+    public function ticketsAction()
+    {
+        $tickets = $this->get('doctrine.orm.entity_manager')
+            ->getRepository('ClarolineBacklogBundle:Ticket')
+            ->findAll();
+
+        return array('tickets' => $tickets);
+    }
+
+    /**
+     * @EXT\Route("/tickets/new", name="new_ticket_form")
+     * @EXT\Method("GET")
+     * @EXT\Template
+     */
+    public function ticketFormAction()
     {
         return array();
     }
