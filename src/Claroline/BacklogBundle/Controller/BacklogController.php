47d27602dc403fd10dcd9be5d3968ea58c920ef2
LaurentG <laurent@sk1.be>
2014-04-02 11:24:09 +0200

    add status form and tickets row

--- src/Claroline/BacklogBundle/Controller/BacklogController.php
+++ src/Claroline/BacklogBundle/Controller/BacklogController.php
@@ -3,6 +3,7 @@
 namespace Claroline\BacklogBundle\Controller;
 
 use Claroline\BacklogBundle\Entity\Ticket;
+use Claroline\BacklogBundle\Entity\Status;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as EXT;
@@ -29,7 +30,11 @@ class BacklogController extends Controller
     public function ticketFormAction(Request $request)
     {
         $form = $this->createFormBuilder()
-            ->add('title', 'text')
+            ->add('title', 'text', array('max_length' => 80, 'required' => true, 'label' => 'Sujet'))
+            ->add('description', 'textarea', array('required' => false, 'label' => 'Description'))
+            ->add('time', 'text', array('max_length' => 80, 'required' => false, 'label' => 'Temps'))
+            ->add('path', 'text', array('max_length' => 80, 'required' => false, 'label' => 'Chemin'))
+            ->add('status', 'collection', array('options' => array('data_class' => 'Claroline\BacklogBundle\Entity\Status')))
             ->add('save', 'submit')
             ->getForm();
 
@@ -42,6 +47,34 @@ class BacklogController extends Controller
                 $ticket->setCreator($this->getUser());
                 $em = $this->get('doctrine.orm.entity_manager');
                 $em->persist($ticket);
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('tickets'));
+            }
+        }
+
+        return array('form' => $form->createView());
+    }
+
+    /**
+     * @EXT\Route("/status/new", name="new_status_form")
+     * @EXT\Template
+     */
+    public function statusFormAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('statusName', 'text', array('max_length' => 80, 'required' => true, 'label' => 'Status'))
+            ->add('save', 'submit')
+            ->getForm();
+
+        if ($request->getMethod() === 'POST') {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $status = new Status();
+                $status->setStatus($form->get('statusName')->getData());
+                $em = $this->get('doctrine.orm.entity_manager');
+                $em->persist($status);
                 $em->flush();
 
                 return $this->redirect($this->generateUrl('tickets'));
